0f55bd91248210f1ee0dfd5871d65ee6421b50a4
CandidateMaster2002 <kickmegreen@gmail.com>
2025-03-15 14:38:35 +0530

    inital commit

--- src/main/java/com/iskcondhanbad/donordash/DonordashApplication.java
+++ src/main/java/com/iskcondhanbad/donordash/DonordashApplication.java
@@ -4,12 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import io.github.cdimascio.dotenv.*;
 
 @CrossOrigin
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class DonordashApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+        System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+        System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+        System.setProperty("RAZORPAY_KEY_ID", dotenv.get("RAZORPAY_KEY_ID"));
+        System.setProperty("RAZORPAY_KEY_SECRET", dotenv.get("RAZORPAY_KEY_SECRET"));
+        System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
+        System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
+
 		SpringApplication.run(DonordashApplication.class, args);
 	}
 
