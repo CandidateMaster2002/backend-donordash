587b50a582b6ee125cc358f85ae356070a32535c
iskdhnaccounts <120113686+iskdhnaccounts@users.noreply.github.com>
2025-03-17 21:47:58 +0530

    Update DonordashApplication.java

--- src/main/java/com/iskcondhanbad/donordash/DonordashApplication.java
+++ src/main/java/com/iskcondhanbad/donordash/DonordashApplication.java
@@ -4,25 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import io.github.cdimascio.dotenv.*;
 
 @CrossOrigin
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class DonordashApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.out.println("dottt"+ dotenv.get("DATABASE_URL"));
-		System.out.println("RAZORPAY_KEY_ID: " + dotenv.get("RAZORPAY_KEY_ID"));
-		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
-        System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
-        System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
-        System.setProperty("RAZORPAY_KEY_ID", dotenv.get("RAZORPAY_KEY_ID"));
-        System.setProperty("RAZORPAY_KEY_SECRET", dotenv.get("RAZORPAY_KEY_SECRET"));
-        System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
-        System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
+		// Access environment variables directly from the system environment
+		System.out.println("DATABASE_URL: " + System.getenv("DATABASE_URL"));
+		System.out.println("RAZORPAY_KEY_ID: " + System.getenv("RAZORPAY_KEY_ID"));
+        
+		// Set system properties if you want to pass them to Spring Boot application
+		System.setProperty("DATABASE_URL", System.getenv("DATABASE_URL"));
+        System.setProperty("DATABASE_USERNAME", System.getenv("DATABASE_USERNAME"));
+        System.setProperty("DATABASE_PASSWORD", System.getenv("DATABASE_PASSWORD"));
+        System.setProperty("RAZORPAY_KEY_ID", System.getenv("RAZORPAY_KEY_ID"));
+        System.setProperty("RAZORPAY_KEY_SECRET", System.getenv("RAZORPAY_KEY_SECRET"));
+        System.setProperty("MAIL_USERNAME", System.getenv("MAIL_USERNAME"));
+        System.setProperty("MAIL_PASSWORD", System.getenv("MAIL_PASSWORD"));
 
 		SpringApplication.run(DonordashApplication.class, args);
 	}
-
 }
